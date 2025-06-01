5b4e055a0d2afd5e6d5f4fa42e2d5d07ec48cda1
gashman878 <gashman878@gmail.com>
2025-06-01 21:40:01 +0100

    fourth

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let timeLeft = 60;
+let timeLeft = 62;
 let timeInterval;
 
 function Submit_Name() {
@@ -11,8 +11,6 @@ function Submit_Name() {
     document.querySelector('.start_exam').disabled = false;
     document.getElementById('Name_space').classList.add('Name_space');
     document.getElementById('examiner').innerHTML = `<p>WELCOME, ${name.value}</p>`;
-
-    
 
 
     name.value = "";
@@ -32,6 +30,38 @@ function examTime() {
 
         if (timeLeft <= 0) {
             clearInterval(timeInterval);
+            submitExam(true);
         }
     }, 1000);
 }
+
+function submitExam(auto = false) {
+    clearInterval(timeInterval);
+    document.getElementById('examForm').querySelectorAll('input').forEach(input =>{input.disabled = true;})
+    const selected = document.querySelectorAll('.myRadio:checked');
+    const result = {};
+
+    selected.forEach(radio => {
+        result[radio.name] = radio.value;
+    });
+    console.log(result);
+    return result;
+
+    const answers = {
+        q1: "4",
+        q2: "Paris",
+        q3: "Abuja",
+        q4: "Dangote",
+        q5: "1963",
+        q6: "Light",
+        q7: "26",
+        q8: "Garri",
+        q9: "Super eagles",
+        q10: "June",
+        q11: "Okada",
+        q12: "Enugu",
+    };
+
+    let score = 0;
+}
+
